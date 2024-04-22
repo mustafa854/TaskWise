229d4975d2cec60ae38a7af497c346a6f87f3b90
Mustafa <mustafabidiwala84@gmail.com>
2024-04-22 20:58:31 +0530

    List team users completed

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,8 @@ from .views import project_board_base, team_base, user_base
 
 urlpatterns = [
     path('users/', user_base.UserView.as_view()),
-    path('users/team', user_base.get_user_teams),
+    path('users/teams', user_base.get_user_teams),
     path('teams/', team_base.TeamView.as_view()),
+    path('teams/users', team_base.list_team_users),
     path('boards/', project_board_base.BoardView.as_view()),
-    # path('teams/member', user_base.UserView.as_view())
 ]
