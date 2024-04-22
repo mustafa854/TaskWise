5b6131c36028d7fdcf50da106741cb7ef0d11279
Mustafa <mustafabidiwala84@gmail.com>
2024-04-22 21:58:12 +0530

    Fixed some close board issues and saved all the closed tasks

--- api/views/project_board_base.py
+++ api/views/project_board_base.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 import json
 import os
+import shutil
 from uuid import uuid4
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -23,6 +24,8 @@ class BoardView(APIView):
       return self.list_boards(request)
     
     def post(self, request):
+      if request.data.get("id"):
+        return self.export_board(request)
       if request.data.get("title") and request.data.get("user_id"):
         return self.add_task(request)
       return self.create_board(request)
@@ -57,7 +60,7 @@ class BoardView(APIView):
           print(json_closed_board)
           for closed_board in json_closed_board:
             if closed_board['name'] == output['name']:
-              return Response({"error":"Please enter a unique Name. The team with the given name exists."}, status=400)
+              return Response({"error":"Please enter a unique Name. The Board with the given name exists."}, status=400)
           
         open_board = open(self.open_board_path+".txt", "w")
         creation_time = str(date.today()) +" " +str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)
@@ -68,6 +71,7 @@ class BoardView(APIView):
       return Response({"id": output['id']})
       
     def close_board(self, request: str) -> str:
+      
       open_tasks_count = len(os.listdir(self.open_tasks_path))
       in_progress_tasks_count = len(os.listdir(self.in_progress_tasks_path))
       if open_tasks_count + in_progress_tasks_count>0:
@@ -78,6 +82,8 @@ class BoardView(APIView):
       with open(self.open_board_path + ".txt", "r") as open_board:
         current_close_board = json.load(open_board)
         # print(self.boards_path)
+      if current_close_board['id'] != str(request.data.get('id')):
+        return Response({"error": "Please enter the correct board id to close the board! "}, 404)
       current_close_board['close_time'] = str(date.today()) +" " +str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)
       current_close_board['status'] = "CLOSED"
       with open(self.boards_path + ".txt") as closed_boards:
@@ -87,9 +93,15 @@ class BoardView(APIView):
         closed_boards.write(json.dumps(output))
       
       closed_tasks = os.listdir(self.closed_tasks_path)
+      os.mkdir(os.path.join(os.path.join(os.getcwd(), "db"), "tasks", "COMPLETED",current_close_board['id'] ))
       if len(closed_tasks)>0:
         for task in closed_tasks:
-          os.remove(os.path.join(self.closed_tasks_path,task ))
+          src = os.path.join(os.getcwd(), "db", "tasks", "CLOSED",task )
+          print(src)
+          dest = os.path.join(os.getcwd(), "db", "tasks", "COMPLETED", current_close_board['id'],task )
+          print(dest)
+          shutil.move(src,dest)
+          
         
       os.remove(self.open_board_path + ".txt")
         
@@ -239,93 +251,7 @@ class ProjectBoardBase:
     """
     A project board is a unit of delivery for a project. Each board will have a set of tasks assigned to a user.
     """
-
-    # create a board
-    def create_board(self, request: str):
-        """
-        :param request: A json string with the board details.
-        {
-            "name" : "<board_name>",
-            "description" : "<description>",
-            "team_id" : "<team id>"
-            "creation_time" : "<date:time when board was created>"
-        }
-        :return: A json string with the response {"id" : "<board_id>"}
-
-        Constraint:
-         * board name must be unique for a team
-         * board name can be max 64 characters
-         * description can be max 128 characters
-        """
-        pass
-
-    # close a board
-    def close_board(self, request: str) -> str:
-        """
-        :param request: A json string with the user details
-        {
-          "id" : "<board_id>"
-        }
-
-        :return:
-
-        Constraint:
-          * Set the board status to CLOSED and record the end_time date:time
-          * You can only close boards with all tasks marked as COMPLETE
-        """
-        pass
-
-    # add task to board
-    def add_task(self, request: str) -> str:
-        """
-        :param request: A json string with the task details. Task is assigned to a user_id who works on the task
-        {
-            "title" : "<board_name>",
-            "description" : "<description>",
-            "user_id" : "<team id>"
-            "creation_time" : "<date:time when task was created>"
-        }
-        :return: A json string with the response {"id" : "<task_id>"}
-
-        Constraint:
-         * task title must be unique for a board
-         * title name can be max 64 characters
-         * description can be max 128 characters
-
-        Constraints:
-        * Can only add task to an OPEN board
-        """
-        pass
-
-    # update the status of a task
-    def update_task_status(self, request: str):
-        """
-        :param request: A json string with the user details
-        {
-            "id" : "<task_id>",
-            "status" : "OPEN | IN_PROGRESS | COMPLETE"
-        }
-        """
-        pass
-
-    # list all open boards for a team
-    def list_boards(self, request: str) -> str:
-        """
-        :param request: A json string with the team identifier
-        {
-          "id" : "<team_id>"
-        }
-
-        :return:
-        [
-          {
-            "id" : "<board_id>",
-            "name" : "<board_name>"
-          }
-        ]
-        """
-        pass
-
+    
     def export_board(self, request: str) -> str:
         """
         Export a board in the out folder. The output will be a txt file.
