80429da17f3646e02ee724219f50fa589c8ee7c9
Mustafa <mustafabidiwala84@gmail.com>
2024-04-23 15:52:39 +0530

    Made endpoints accessible to both with '/' and without

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,14 @@ from .views import project_board_base, team_base, user_base
 
 urlpatterns = [
     path('users/', user_base.UserView.as_view()),
+    path('users', user_base.UserView.as_view()),
     path('users/teams', user_base.get_user_teams),
+    path('users/teams/', user_base.get_user_teams),
     path('teams/', team_base.TeamView.as_view()),
+    path('teams', team_base.TeamView.as_view()),
     path('teams/users', team_base.list_team_users),
+    path('teams/users/', team_base.list_team_users),
     path('boards/', project_board_base.BoardView.as_view()),
+    path('boards', project_board_base.BoardView.as_view()),
   
 ]
